2f2ca1ec33d1671d8a5e431965e4b84a1a513a84
gshanko125298 <gdekebo2020@gmail.com>
2022-09-25 02:50:02 +0300

    Update_project_file

--- database/airflow.py
+++ database/airflow.py
@@ -1,29 +1,35 @@
-from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.operators import LoadDimensionOpera
-import SqlQueries
+'''
+This script helps to upload data to S3
+Customize variables:
+* source_file_path
+* target_bucket
+* target_key
+and make sure that your AWS credentials in config.cfg are up to date.
+'''
 
 
-def load_dimension_subdag(
-    parent_dag_name,
-    task_id,
-    redshift_conn_id,
-    sql_statement,
-    delete_load,
-    table_name,
-    *args, **kwargs):
-    
-    dag = DAG(f"{parent_dag_name}.{task_id}", **kwargs)
-    
-    load_dimension_table = LoadDimensionOperator(
-        task_id=task_id,
-        dag=dag,
-        redshift_conn_id=redshift_conn_id,
-        sql_query = sql_statement,
-        delete_load = delete_load,
-        table_name = table_name,
-    )    
-    
-    load_dimension_table
-    
-    return dag
+import boto3
+import configparser
+
+config = configparser.ConfigParser()
+config.read('../../config.cfg')
+aws = config['AWS']
+
+KEY = aws['KEY']
+SECRET = aws['SECRET']
+
+s3 = boto3.client(
+    's3',
+    region_name='eu-west-1',
+    aws_access_key_id=KEY,
+    aws_secret_access_key=SECRET
+)
+
+
+
+source_file_path = "C:\Users\Genet Shanko\A_startUp\A_Startup_Data-_Warehouse-\Dataset\DATA_20181030_d10_0830_0900 (1).csv"
+target_bucket = "Data engineering"
+target_key = "input_data/GlobalLandTemperaturesByCountry.csv"
+
+
+s3.upload_file(source_file_path, target_bucket, target_key)
